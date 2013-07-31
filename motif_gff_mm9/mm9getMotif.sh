fc44964413c37fce76d7dca5330bed9cd56337bb
Xi Chen <xc406@steve.(none)>
2013-07-31 13:59:49 -0400

    shell script to select and write a certain type of motifs to a new folder

---
+++ motif_gff_mm9/mm9getMotif.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+FILES=/home/xc406/data/mm9motifs80/pwms_all_motifs/motifoutput/*
+TFFILES=/home/xc406/data/mm9motifs80/mappable/*.txt
+for tf in $TFFILES
+do 
+    tfpath=${tf%/*}
+    tfdir=${tf%.*}
+    #echo $tfdir
+    tfname="${tfdir##*/}"
+    #echo $tfname
+    #mkdir $tfdir #/home/xc406/data/Homo_sapiens_2013_02_20_3-54_pm/motifsPerTf/${tfname}mo
+    for f in $FILES
+    do  
+        filename=${f%.*}
+        name="${filename##*/}"
+  
+        for i in $(< ${tf})
+        do
+	    if [[ "$name" == *"$i"* ]]; then
+	        echo "Im here!"
+	        cp $f $tfpath/motifoutput/
+	    fi   
+        done
+    done
+    #uniprobe2meme -bg ~/data/hg19bgfile $tfdir/*.txt > $tfpath/hg19${tfname}.meme
+done
+
